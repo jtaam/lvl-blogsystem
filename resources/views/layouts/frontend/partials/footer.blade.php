8b41cd71c9af7dbdfd0a71c0c74fb89d430fa516
Tam Jid <tamjid.mdh@gmail.com>
2018-10-03 02:11:40 +0600

    frontend home images

--- resources/views/layouts/frontend/partials/footer.blade.php
+++ resources/views/layouts/frontend/partials/footer.blade.php
@@ -6,7 +6,7 @@
             <div class="col-lg-4 col-md-6">
                 <div class="footer-section">
 
-                    <a class="logo" href="#"><img src="images/logo.png" alt="Logo Image"></a>
+                    <a class="logo" href="#"><img src="{{asset('assets/frontend/images/logo.png')}}" alt="{{config('app.name')}}"></a>
                     <p class="copyright">Bona @ 2017. All rights reserved.</p>
                     <p class="copyright">Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
                     <ul class="icons">
