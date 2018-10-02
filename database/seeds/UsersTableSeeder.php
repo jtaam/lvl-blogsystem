94cce8b5120c0f1c07dcb5bcc08a5923ad596f20
Tam Jid <tamjid.mdh@gmail.com>
2018-10-02 09:38:01 +0600

    database seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'role_id'=>'1',
+            'name'=>'Mr Admin',
+            'username'=>'admin',
+            'email'=>'admin@example.net',
+            'password'=> bcrypt('admin'),
+        ]);
+        DB::table('users')->insert([
+            'role_id'=>'2',
+            'name'=>'Mr Author',
+            'username'=>'author',
+            'email'=>'author@example.net',
+            'password'=> bcrypt('author'),
+        ]);
+    }
+}
