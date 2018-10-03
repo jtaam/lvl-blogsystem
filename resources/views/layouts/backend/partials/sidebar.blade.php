fcbd9dc3518435c4788c5fc3c4257507ef563aef
Tam Jid <tamjid.mdh@gmail.com>
2018-10-03 06:32:20 +0600

    admin dashboard

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -2,11 +2,11 @@
     <!-- User Info -->
     <div class="user-info">
         <div class="image">
-            <img src="images/user.png" width="48" height="48" alt="User" />
+            <img src="{{asset('assets/backend/images/user.png')}}" width="48" height="48" alt="User" />
         </div>
         <div class="info-container">
-            <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
-            <div class="email">john.doe@example.com</div>
+            <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{Auth::user()->name}}</div>
+            <div class="email">{{Auth::user()->email}}</div>
             <div class="btn-group user-helper-dropdown">
                 <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                 <ul class="dropdown-menu pull-right">
@@ -27,68 +27,10 @@
         <ul class="list">
             <li class="header">MAIN NAVIGATION</li>
             <li class="active">
-                <a href="index.html">
+                <a href="{{route('admin.dashboard')}}">
                     <i class="material-icons">home</i>
                     <span>Home</span>
                 </a>
-            </li>
-            <li>
-                <a href="pages/typography.html">
-                    <i class="material-icons">text_fields</i>
-                    <span>Typography</span>
-                </a>
-            </li>
-            <li>
-                <a href="pages/helper-classes.html">
-                    <i class="material-icons">layers</i>
-                    <span>Helper Classes</span>
-                </a>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="menu-toggle">
-                    <i class="material-icons">widgets</i>
-                    <span>Widgets</span>
-                </a>
-                <ul class="ml-menu">
-                    <li>
-                        <a href="javascript:void(0);" class="menu-toggle">
-                            <span>Cards</span>
-                        </a>
-                        <ul class="ml-menu">
-                            <li>
-                                <a href="pages/widgets/cards/basic.html">Basic</a>
-                            </li>
-                            <li>
-                                <a href="pages/widgets/cards/colored.html">Colored</a>
-                            </li>
-                            <li>
-                                <a href="pages/widgets/cards/no-header.html">No Header</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);" class="menu-toggle">
-                            <span>Infobox</span>
-                        </a>
-                        <ul class="ml-menu">
-                            <li>
-                                <a href="pages/widgets/infobox/infobox-1.html">Infobox-1</a>
-                            </li>
-                            <li>
-                                <a href="pages/widgets/infobox/infobox-2.html">Infobox-2</a>
-                            </li>
-                            <li>
-                                <a href="pages/widgets/infobox/infobox-3.html">Infobox-3</a>
-                            </li>
-                            <li>
-                                <a href="pages/widgets/infobox/infobox-4.html">Infobox-4</a>
-                            </li>
-                            <li>
-                                <a href="pages/widgets/infobox/infobox-5.html">Infobox-5</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
             </li>
             <li>
                 <a href="javascript:void(0);" class="menu-toggle">
@@ -168,196 +110,7 @@
                     </li>
                 </ul>
             </li>
-            <li>
-                <a href="javascript:void(0);" class="menu-toggle">
-                    <i class="material-icons">assignment</i>
-                    <span>Forms</span>
-                </a>
-                <ul class="ml-menu">
-                    <li>
-                        <a href="pages/forms/basic-form-elements.html">Basic Form Elements</a>
-                    </li>
-                    <li>
-                        <a href="pages/forms/advanced-form-elements.html">Advanced Form Elements</a>
-                    </li>
-                    <li>
-                        <a href="pages/forms/form-examples.html">Form Examples</a>
-                    </li>
-                    <li>
-                        <a href="pages/forms/form-validation.html">Form Validation</a>
-                    </li>
-                    <li>
-                        <a href="pages/forms/form-wizard.html">Form Wizard</a>
-                    </li>
-                    <li>
-                        <a href="pages/forms/editors.html">Editors</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="menu-toggle">
-                    <i class="material-icons">view_list</i>
-                    <span>Tables</span>
-                </a>
-                <ul class="ml-menu">
-                    <li>
-                        <a href="pages/tables/normal-tables.html">Normal Tables</a>
-                    </li>
-                    <li>
-                        <a href="pages/tables/jquery-datatable.html">Jquery Datatables</a>
-                    </li>
-                    <li>
-                        <a href="pages/tables/editable-table.html">Editable Tables</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="menu-toggle">
-                    <i class="material-icons">perm_media</i>
-                    <span>Medias</span>
-                </a>
-                <ul class="ml-menu">
-                    <li>
-                        <a href="pages/medias/image-gallery.html">Image Gallery</a>
-                    </li>
-                    <li>
-                        <a href="pages/medias/carousel.html">Carousel</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="menu-toggle">
-                    <i class="material-icons">pie_chart</i>
-                    <span>Charts</span>
-                </a>
-                <ul class="ml-menu">
-                    <li>
-                        <a href="pages/charts/morris.html">Morris</a>
-                    </li>
-                    <li>
-                        <a href="pages/charts/flot.html">Flot</a>
-                    </li>
-                    <li>
-                        <a href="pages/charts/chartjs.html">ChartJS</a>
-                    </li>
-                    <li>
-                        <a href="pages/charts/sparkline.html">Sparkline</a>
-                    </li>
-                    <li>
-                        <a href="pages/charts/jquery-knob.html">Jquery Knob</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="menu-toggle">
-                    <i class="material-icons">content_copy</i>
-                    <span>Example Pages</span>
-                </a>
-                <ul class="ml-menu">
-                    <li>
-                        <a href="pages/examples/sign-in.html">Sign In</a>
-                    </li>
-                    <li>
-                        <a href="pages/examples/sign-up.html">Sign Up</a>
-                    </li>
-                    <li>
-                        <a href="pages/examples/forgot-password.html">Forgot Password</a>
-                    </li>
-                    <li>
-                        <a href="pages/examples/blank.html">Blank Page</a>
-                    </li>
-                    <li>
-                        <a href="pages/examples/404.html">404 - Not Found</a>
-                    </li>
-                    <li>
-                        <a href="pages/examples/500.html">500 - Server Error</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="menu-toggle">
-                    <i class="material-icons">map</i>
-                    <span>Maps</span>
-                </a>
-                <ul class="ml-menu">
-                    <li>
-                        <a href="pages/maps/google.html">Google Map</a>
-                    </li>
-                    <li>
-                        <a href="pages/maps/yandex.html">YandexMap</a>
-                    </li>
-                    <li>
-                        <a href="pages/maps/jvectormap.html">jVectorMap</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:void(0);" class="menu-toggle">
-                    <i class="material-icons">trending_down</i>
-                    <span>Multi Level Menu</span>
-                </a>
-                <ul class="ml-menu">
-                    <li>
-                        <a href="javascript:void(0);">
-                            <span>Menu Item</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);">
-                            <span>Menu Item - 2</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);" class="menu-toggle">
-                            <span>Level - 2</span>
-                        </a>
-                        <ul class="ml-menu">
-                            <li>
-                                <a href="javascript:void(0);">
-                                    <span>Menu Item</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0);" class="menu-toggle">
-                                    <span>Level - 3</span>
-                                </a>
-                                <ul class="ml-menu">
-                                    <li>
-                                        <a href="javascript:void(0);">
-                                            <span>Level - 4</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="pages/changelogs.html">
-                    <i class="material-icons">update</i>
-                    <span>Changelogs</span>
-                </a>
-            </li>
-            <li class="header">LABELS</li>
-            <li>
-                <a href="javascript:void(0);">
-                    <i class="material-icons col-red">donut_large</i>
-                    <span>Important</span>
-                </a>
-            </li>
-            <li>
-                <a href="javascript:void(0);">
-                    <i class="material-icons col-amber">donut_large</i>
-                    <span>Warning</span>
-                </a>
-            </li>
-            <li>
-                <a href="javascript:void(0);">
-                    <i class="material-icons col-light-blue">donut_large</i>
-                    <span>Information</span>
-                </a>
-            </li>
+
         </ul>
     </div>
     <!-- #Menu -->
