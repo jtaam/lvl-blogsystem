140cd073861638fe41ad7f397606d18067c69a29
Tam Jid <tamjid.mdh@gmail.com>
2018-10-05 03:40:39 +0600

    post C 2

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Category;
 use App\Post;
 use App\Tag;
+use Brian2694\Toastr\Facades\Toastr;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class PostController extends Controller
 {
@@ -48,6 +53,40 @@ class PostController extends Controller
             'tags' => 'required',
             'body' => 'required'
         ]);
+        $image = $request->file('image');
+        $slug = str_slug($request->title);
+        if (isset($image)){
+//            make unique name image
+            $currentDate = Carbon::now()->toDateString();
+            $imageName = $slug.'-'.$currentDate.''.uniqid().'.'.$image->getClientOriginalExtension();
+            if (!Storage::disk('public')->exists('post')){
+                Storage::disk('public')->makeDirectory('post');
+            }
+            $resizeImage = Image::make($image)->resize(1600,1066)->save();
+            Storage::disk('public')->put('post/'.$imageName,$resizeImage);
+        }else{
+            $imageName='default.png';
+        }
+        $post= new Post();
+        $post->user_id=Auth::id();
+        $post->title=$request->title;
+        $post->slug=$slug;
+        $post->image=$imageName;
+        $post->body=$request->body;
+        if (isset($request->status)){
+            $post->status=true;
+        }else{
+            $post->status=false;
+        }
+        $post->is_approved=true;
+        $post->save();
+
+        $post->categories()->attach($request->categories);
+        $post->tags()->attach($request->tags);
+
+        Toastr::success('Post saved successfully!');
+
+        return redirect()->route('admin.post.index');
     }
 
     /**
