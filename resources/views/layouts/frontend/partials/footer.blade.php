73656e9cd89502dcb7d24bf6ec139517d688fc45
Tam Jid <tamjid.mdh@gmail.com>
2018-10-07 01:06:30 +0600

    footer categories

--- resources/views/layouts/frontend/partials/footer.blade.php
+++ resources/views/layouts/frontend/partials/footer.blade.php
@@ -24,14 +24,9 @@
                 <div class="footer-section">
                     <h4 class="title"><b>CATAGORIES</b></h4>
                     <ul>
-                        <li><a href="#">BEAUTY</a></li>
-                        <li><a href="#">HEALTH</a></li>
-                        <li><a href="#">MUSIC</a></li>
-                    </ul>
-                    <ul>
-                        <li><a href="#">SPORT</a></li>
-                        <li><a href="#">DESIGN</a></li>
-                        <li><a href="#">TRAVEL</a></li>
+                        @foreach($categories as $category)
+                        <li><a href="#">{{strtoupper($category->name)}}</a></li>
+                        @endforeach
                     </ul>
                 </div><!-- footer-section -->
             </div><!-- col-lg-4 col-md-6 -->
