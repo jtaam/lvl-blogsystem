604e505215da9c6123c958e146594eb06c5dc9b0
Tam Jid <tamjid.mdh@gmail.com>
2018-10-11 01:14:32 +0600

    admin dashboard index 2

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -60,6 +60,100 @@
         </div>
         <!-- #END# Widgets -->
 
+        <!-- Widgets -->
+        <div class="row clearfix">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <div class="info-box bg-blue-grey hover-zoom-effect">
+                    <div class="icon">
+                        <i class="material-icons">apps</i>
+                    </div>
+                    <div class="content">
+                        <div class="text">CATEGORIES</div>
+                        <div class="number count-to" data-from="0" data-to="{{$category_count}}" data-speed="700" data-fresh-interval="20"></div>
+                    </div>
+                </div>
+
+                <div class="info-box bg-amber hover-zoom-effect">
+                    <div class="icon">
+                        <i class="material-icons">labels</i>
+                    </div>
+                    <div class="content">
+                        <div class="text">TAGS</div>
+                        <div class="number count-to" data-from="0" data-to="{{$tag_count}}" data-speed="700" data-fresh-interval="20"></div>
+                    </div>
+                </div>
+
+                <div class="info-box bg-brown hover-zoom-effect">
+                    <div class="icon">
+                        <i class="material-icons">supervised_user_circle</i>
+                    </div>
+                    <div class="content">
+                        <div class="text">AUTHORS</div>
+                        <div class="number count-to" data-from="0" data-to="{{$authors_count}}" data-speed="700" data-fresh-interval="20"></div>
+                    </div>
+                </div>
+
+                <div class="info-box bg-purple hover-zoom-effect">
+                    <div class="icon">
+                        <i class="material-icons">fiber_new</i>
+                    </div>
+                    <div class="content">
+                        <div class="text">TODAY AUTHORS</div>
+                        <div class="number count-to" data-from="0" data-to="{{$new_authors_today}}" data-speed="700" data-fresh-interval="20"></div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-9">
+                <div class="card">
+                    <div class="header">
+                        <h2>MOST POPULAR POSTS</h2>
+                    </div>
+                    <div class="body">
+                        <table class="table table-responsive dashboard-task-infos">
+                            <thead>
+                                <tr>
+                                    <th>Rank</th>
+                                    <th>Title</th>
+                                    <th>Author</th>
+                                    <th>Views</th>
+                                    <th>Favorite</th>
+                                    <th>Comments</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($popular_posts as $key=>$post)
+                                <tr>
+                                    <td>{{$key+1}}</td>
+                                    <td>{{str_limit($post->title,20)}}</td>
+                                    <td>{{$post->user->name}}</td>
+                                    <td>{{$post->view_count}}</td>
+                                    <td>{{$post->favorite_posts_count}}</td>
+                                    <td>{{$post->comments_count}}</td>
+                                    <td>
+                                        @if($post->status == true)
+                                            <span class="label bg-green">Published</span>
+                                        @else
+                                            <span class="label bg-red">Pending</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <a href="{{route('post.details',$post->slug)}}" class="btn btn-sm btn-primary" target="_blank">
+                                            <i class="material-icons">visibility</i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- #END# Widgets -->
+
         <div class="row clearfix">
             <!-- Task Info -->
             <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
