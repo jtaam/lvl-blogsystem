7161e51f138f0cce701f4b148c9129826a8e7911
_tam <tamjid84@yahoo.com>
2018-12-24 09:04:30 +0600

    posts fix

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.frontend.app')
 
 @section('title')
-{{ucwords($post->title)}}
+    {{ucwords($post->title)}}
 @endsection
 
 @push('css')
@@ -11,8 +11,12 @@
         .favorite_posts {
             color: blue;
         }
+
         .slider {
-            background-image: url({{$post->categories[0]->image}}) !important;
+            @if($post)
+             background-image: url({{$post->categories[0]->image}}) !important;
+        @endif
+
         }
     </style>
 @endpush
@@ -21,89 +25,102 @@
     <div class="slider">
         <div class="display-table  center-text">
             <h1 class="title display-table-cell">
-                <b>{{ucwords($post->categories[0]->name)}}</b>
+                @if($post)
+
+                    <b>{{ucwords($post->categories[0]->name)}}</b>
+                @endif
+
             </h1>
         </div>
     </div><!-- slider -->
 
     <section class="post-area section">
-        <div class="container">
+        @if($post)
 
-            <div class="row">
+            <div class="container">
 
-                <div class="col-lg-8 col-md-12 no-right-padding">
+                <div class="row">
 
-                    <div class="main-post">
+                    <div class="col-lg-8 col-md-12 no-right-padding">
 
-                        <div class="blog-post-inner">
+                        <div class="main-post">
 
-                            <div class="post-info">
+                            <div class="blog-post-inner">
 
-                                <div class="left-area">
-                                    <a class="avatar" href="{{route('author.profile',$post->user->username)}}"><img src="{{ Storage::disk('public')->url('profile/'.$post->user->image) }}" alt="{{$post->user->name}}"></a>
+                                <div class="post-info">
+
+                                    <div class="left-area">
+                                        <a class="avatar" href="{{route('author.profile',$post->user->username)}}"><img
+                                                    src="{{ Storage::disk('public')->url('profile/'.$post->user->image) }}"
+                                                    alt="{{$post->user->name}}"></a>
+                                    </div>
+
+                                    <div class="middle-area">
+                                        <a class="name"
+                                           href="{{route('author.profile',$post->user->username)}}"><b>{{$post->user->name}}</b></a>
+                                        <h6 class="date">on {{$post->created_at->format('M d, Y')}}
+                                            at {{$post->created_at->format('g:ia')}}</h6>
+                                    </div>
+
+                                </div><!-- post-info -->
+
+                                {{--<h3 class="title"><a href="#"><b>{{ucwords($post->title)}}</b></a></h3>--}}
+                                <h3 class="title"><b>{{ucwords($post->title)}}</b></h3>
+
+                                <p class="para">
+                                    {!! $post->post_promo !!}
+                                </p>
+                                <br>
+                                <div class="post-image"><img src="{{ $post->image }}" alt="{{$post->title}}"/></div>
+
+                                <div class="para">
+                                    {!! $post->body !!}
                                 </div>
 
-                                <div class="middle-area">
-                                    <a class="name" href="{{route('author.profile',$post->user->username)}}"><b>{{$post->user->name}}</b></a>
-                                    <h6 class="date">on {{$post->created_at->format('M d, Y')}}
-                                        at {{$post->created_at->format('g:ia')}}</h6>
+                                <div class="tag-area">
+                                    @include('post-includes.tag')
                                 </div>
+                            </div><!-- blog-post-inner -->
 
-                            </div><!-- post-info -->
+                            <div class="post-icons-area">
+                                @include('post-includes.icon')
 
-                            {{--<h3 class="title"><a href="#"><b>{{ucwords($post->title)}}</b></a></h3>--}}
-                            <h3 class="title"><b>{{ucwords($post->title)}}</b></h3>
+                                @include('post-includes.share')
+                            </div>
 
-                            <p class="para">
-                                {!! $post->post_promo !!}
-                            </p>
-                            <br>
-                            <div class="post-image"><img src="{{ $post->image }}" alt="{{$post->title}}" /></div>
+                        </div><!-- main-post -->
+                    </div><!-- col-lg-8 col-md-12 -->
 
-                            <div class="para">
-                                {!! $post->body !!}
+                    <div class="col-lg-4 col-md-12 no-left-padding">
+                        <div class="single-post info-area">
+
+                            <div class="sidebar-area about-area">
+                                <h4 class="title"><b>ABOUT AUTHOR</b></h4>
+                                <p>{{str_limit($post->user->about,200,'...')}}</p>
                             </div>
 
                             <div class="tag-area">
-                                @include('post-includes.tag')
-                            </div>
-                        </div><!-- blog-post-inner -->
 
-                        <div class="post-icons-area">
-                            @include('post-includes.icon')
+                                <h4 class="title"><b>CATEGORIES</b></h4>
+                                <ul>
+                                    @foreach($post->categories as $category)
+                                        <li>
+                                            <a href="{{route('category.posts',$category->slug)}}">{{ucwords($category->name)}}</a>
+                                        </li>
+                                    @endforeach
+                                </ul>
 
-                            @include('post-includes.share')
-                        </div>
+                            </div><!-- subscribe-area -->
 
-                    </div><!-- main-post -->
-                </div><!-- col-lg-8 col-md-12 -->
+                        </div><!-- info-area -->
 
-                <div class="col-lg-4 col-md-12 no-left-padding">
-                    <div class="single-post info-area">
+                    </div><!-- col-lg-4 col-md-12 -->
 
-                        <div class="sidebar-area about-area">
-                            <h4 class="title"><b>ABOUT AUTHOR</b></h4>
-                            <p>{{str_limit($post->user->about,200,'...')}}</p>
-                        </div>
+                </div><!-- row -->
 
-                        <div class="tag-area">
+            </div><!-- container -->
+        @endif
 
-                            <h4 class="title"><b>CATEGORIES</b></h4>
-                            <ul>
-                                @foreach($post->categories as $category)
-                                    <li><a href="{{route('category.posts',$category->slug)}}">{{ucwords($category->name)}}</a></li>
-                                @endforeach
-                            </ul>
-
-                        </div><!-- subscribe-area -->
-
-                    </div><!-- info-area -->
-
-                </div><!-- col-lg-4 col-md-12 -->
-
-            </div><!-- row -->
-
-        </div><!-- container -->
     </section><!-- post-area -->
 
 
