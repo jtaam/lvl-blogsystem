493c8a93cd54a9ba6156b922acd22252f011d70d
_tam <tamjid84@yahoo.com>
2018-12-24 06:52:49 +0600

    production settings

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'tamjid-it.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'tamjidit_laravel_blogsystem'),
+            'username' => env('DB_USERNAME', 'tamjidit_laravel_blogsystem'),
+            'password' => env('DB_PASSWORD', 'n3RIO9ZA+D[0'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
