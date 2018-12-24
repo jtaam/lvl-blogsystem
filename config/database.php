f5654c6014c2e9ee50afecadd1b5b2f19695cb23
_tam <tamjid84@yahoo.com>
2018-12-24 07:02:09 +0600

    database port change

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'tamjid-it.com'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '2082'),
             'database' => env('DB_DATABASE', 'tamjidit_laravel_blogsystem'),
             'username' => env('DB_USERNAME', 'tamjidit_laravel_blogsystem'),
             'password' => env('DB_PASSWORD', 'n3RIO9ZA+D[0'),
