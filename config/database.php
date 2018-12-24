f5e6652e3ec7d7f1c6d9cb18e92ebad421e5f1f5
_tam <tamjid84@yahoo.com>
2018-12-24 07:16:16 +0600

    sqlite

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,10 +42,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'tamjid-it.com'),
-            'port' => env('DB_PORT', '2082'),
-            'database' => env('DB_DATABASE', 'tamjidit_laravel_blogsystem'),
-            'username' => env('DB_USERNAME', 'tamjidit_laravel_blogsystem'),
-            'password' => env('DB_PASSWORD', 'n3RIO9ZA+D[0'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'localhost'),
+            'username' => env('DB_USERNAME', 'localhost'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
